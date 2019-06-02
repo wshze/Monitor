9bc650dc26f9654f41defcf226170158cd820d5f
wshze <42234694+wshze@users.noreply.github.com>
2019-06-02 17:14:03 +0800

    Add files via upload

---
+++ zabbix-police/police.py
@@ -0,0 +1,9 @@
+﻿#/usr/local/zabbix/share/zabbix/alertscripts/police.py
+#https://www.jianshu.com/p/b29cf0682b58
+#!/usr/bin/env python
+#coding:utf-8
+import redis
+import sys
+subject=sys.argv[1]
+r = redis.StrictRedis(host='**.**.**.**', port=6379)
+r.set(subject,subject)
